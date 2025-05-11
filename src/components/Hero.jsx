9b7959e7758efe4dd81d00cc46a2817be7a18901
omoshronny <itsomondironald@gmail.com>
2025-05-12 01:57:46 +0300

    message

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Ronald</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop user interfaces, Sofware applicatons and web applications.
+            I develop user interfaces, Software applicatons and web applications.
           </p>
         </div>
       </div>
